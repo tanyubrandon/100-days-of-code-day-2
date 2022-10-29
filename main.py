0e47f3faea065e959d9776d510ec8aa589267bfd
Tanyu Brandon <82066029+tanyubrandon@users.noreply.github.com>
2022-10-29 00:46:47 +0000

    variable manipulation

---
+++ main.py
@@ -0,0 +1,18 @@
+# Getting to know you
+
+yourName = input("What's is your name?: ")
+favFood = input("What's your favourite food? ")
+favMusic = input("What is your Favourite music? ")
+whereYouLive = input("Where do you live? ")
+print("")
+
+print("You are")
+print(yourName)
+print("You are probably hungry for")
+print(favFood)
+print("and you're definitely getting your groove onto")
+print(favMusic)
+print("Living in the amazing")
+print(whereYouLive)
+# var_names = [yourName,favFood,favMusic,whereYouLive]
+# [print(i) for i in var_names]
